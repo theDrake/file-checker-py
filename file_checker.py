e7847aa2ddab18ecea36b64b10f7f0d3a765338f
David C. Drake <dcdrake42@gmail.com>
2014-09-23 14:42:45 -0600

    Update "file_checker.py", remove "tabs.py"
    Moved tab-replacement code into "file_checker.py", made some additional
    modifications to "file_checker.py", and deleted "tabs.py".

--- file_checker.py
+++ file_checker.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
 
+import sys
+
+
+def ReplaceTabsWithSpaces(filename, spaces_per_tab):
+    tab_count = 0
+    f = open(filename, 'rw')
+    lines = f.readlines()
+    f.close()
+    for line in lines:
+        if line.contains('\t'):
+            tab_count += 1
+
+
 def CheckLineLengths(filename, max_length=80):
-    num_lines_exceeding_max_length = 0
     fin = open(filename, 'r')
     lines = fin.readlines()
+    fin.close()
+    lines_exceeding_max_length = []
     for line in lines:
         if line.length() > max_length:
             num_lines_exceeding_max_length += 1
-    fin.close()
     print 'File "' + filename + '" contains ' + \
           num_lines_exceeding_max_length + ' lines exceeding ' + max_length + \
           ' characters.'
     return num_lines_exceeding_max_length
 
+
 def main():
-    CheckLineLengths(filename)
+    for len(sys.argv) > 1:
+        CheckLineLengths(sys.argv[1])
+
 
 if __name__ == "__main__":
     main()
